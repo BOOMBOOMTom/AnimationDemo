c6e7bda07f697863bf0e3fb48108467809d39b1b
qiao <>
2019-06-02 15:34:30 +0800

    gcfd demo2

--- Demo/VCS/碎片效果/FadeAnimationViewController.m
+++ Demo/VCS/碎片效果/FadeAnimationViewController.m
@@ -42,44 +42,78 @@
 //        NSLog(@"cancel");
 //    }) start];
     
-    // GCD demo
-    dispatch_group_t group = dispatch_group_create();
-    NSUInteger count = [NSProcessInfo processInfo].processorCount;
-    dispatch_queue_t queue = dispatch_queue_create("com.demo1", DISPATCH_QUEUE_SERIAL);
-    dispatch_queue_t gloab = dispatch_queue_create("com.demo", DISPATCH_QUEUE_CONCURRENT);
-    dispatch_semaphore_t sema = dispatch_semaphore_create(count * 2);
+    // GCD demo 2
+//    dispatch_group_t group = dispatch_group_create();
+//    NSUInteger count = [NSProcessInfo processInfo].processorCount;
+//    dispatch_queue_t queue = dispatch_queue_create("com.demo1", DISPATCH_QUEUE_SERIAL);
+//    dispatch_queue_t gloab = dispatch_queue_create("com.demo", DISPATCH_QUEUE_CONCURRENT);
+//    dispatch_semaphore_t sema = dispatch_semaphore_create(count * 2);
+//
+//    for (int i = 0; i < 10;  i ++) {
+//
+//        // 1 semaphore + serial
+//        // 2 group (enter + leave)
+//
+//        dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+//        dispatch_group_async(group, queue, ^{
+//            NSLog(@"----- i ---- %d",i);
+//            dispatch_group_async(group, gloab, ^{
+//
+//
+//                sleep(1);
+//
+//                dispatch_semaphore_signal(sema);
+//                NSLog(@"----- i ---- %d -- 22222 ",i);
+//
+////                dispatch_group_async(group, queue, ^{
+////                    dispatch_semaphore_signal(sema);
+////                    NSLog(@"----- i ---- %d -- 22222 ",i);
+////                });
+//
+//            });
+//
+//        });
+//    }
+//
+//    dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+//    NSLog(@"3333333333");
     
-    for (int i = 0; i < 10;  i ++) {
+    
+    // GCD demo 2
+    
+    // dispatch_group_wait 为同步 会阻塞主线程
+ 
+    dispatch_queue_t queue = dispatch_queue_create("111.com", DISPATCH_QUEUE_SERIAL);
+    dispatch_async(queue, ^{
         
-        // 1 semaphore + serial
-        // 2 group (enter + leave)
+        dispatch_group_t group = dispatch_group_create();
         
-        dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
-        dispatch_group_async(group, queue, ^{
-            NSLog(@"----- i ---- %d",i);
-            dispatch_group_async(group, gloab, ^{
-                
-                
-                sleep(1);
-                
-                dispatch_semaphore_signal(sema);
-                NSLog(@"----- i ---- %d -- 22222 ",i);
-        
-//                dispatch_group_async(group, queue, ^{
-//                    dispatch_semaphore_signal(sema);
-//                    NSLog(@"----- i ---- %d -- 22222 ",i);
-//                });
-                
+        //dispatch_group_enter(group);
+        dispatch_group_async(group, dispatch_get_global_queue(0, 0), ^{
+            
+            //dispatch_group_leave(group);
+            
+            // dispatch_group_async里放同步代码，会直接跳过，然后执行dispatch_group_notify(不等待执行完)
+            
+//            dispatch_async(dispatch_queue_create("22222", DISPATCH_QUEUE_SERIAL), ^{
+//                sleep(2);
+//
+//                NSLog(@"222222222");
+//            });
+            
+            // dispatch_group_async里放异步代码，会等待执行完，然后执行dispatch_group_notify
+            
+            dispatch_sync(dispatch_get_global_queue(0, 0), ^{
+                sleep(2);
+                NSLog(@"222222222");
             });
             
         });
-    }
-
-    dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
-    NSLog(@"3333333333");
-    
-    
-    
+        
+        dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+        NSLog(@"333333333");
+    });
+    NSLog(@"111111111");
     
 }
 
